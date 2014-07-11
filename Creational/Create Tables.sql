3edea9097259b4fd353327236b036cc2c50ec260
pnakibar <pedronakibar@gmail.com>
2014-07-11 20:34:39 -0300

    criar tabelas atualizado

--- Creational/Create Tables.sql
+++ Creational/Create Tables.sql
@@ -1,10 +1,36 @@
-CREATE TABLE jogador
-(
+CREATE TABLE jogador(
   jogador_id serial NOT NULL PRIMARY KEY,
   username character varying(50) UNIQUE NOT NULL,
   realname character varying(50) NOT NULL,
   password character varying(50) NOT NULL,
   email character varying(355) UNIQUE NOT NULL
+);
+
+CREATE TABLE inimigo(
+  inimigo_id serial NOT NULL PRIMARY KEY,
+  nomeinimigo character varying(50) UNIQUE NOT NULL,
+  recompensa integer NOT NULL
+);
+
+CREATE TABLE tripulante(
+  tripulante_id serial NOT NULL PRIMARY KEY,
+  tripulantename character varying(50) NOT NULL,
+  idade integer NOT NULL,
+  bio character varying(355),
+  vida integer NOT NULL = 100,
+  funcao_id serial NOT NULL
+);
+
+CREATE TABLE no(
+  no_id serial NOT NULL PRIMARY KEY,
+  coordanada_no int NOT NULL,
+  loja_no serial,
+);  
+
+CREATE TABLE loja_no(
+  loja_id serial NOT NULL PRIMARY KEY,
+  mensagem character varying(355),
+  produtos character varying(355)
 );
 
 CREATE TABLE tiponave(
@@ -15,12 +41,27 @@ CREATE TABLE tiponave(
 );
 
 CREATE TABLE nave(
-    nave_id serial PRIMARY KEY,
-    tiponave_id serial UNIQUE NOT NULL,
-    nomenave VARCHAR (50) NOT NULL
+  nave_id serial PRIMARY KEY,
+  tiponave_id serial UNIQUE NOT NULL,
+  nomenave VARCHAR (50) NOT NULL
+);
+
+CREATE TABLE funcao(
+  funcao_id serial UNIQUE NOT NULL,
+  funcao_nome character varying(50) NOT NULL
 );
 
 CREATE TABLE navejogador(
-    jogador_id serial NOT NULL PRIMARY KEY,
-    nave_id serial NOT NULL 
+  jogador_id serial NOT NULL PRIMARY KEY,
+  nave_id serial NOT NULL 
+);
+
+CREATE TABLE naveinimigo(
+  inimigo_id serial NOT NULL PRIMARY KEY,
+  nave_id serial NOT NULL 
+);
+
+CREATE TABLE tripulantesdeumanave(
+  nave_id serial NOT NULL PRIMARY KEY,
+  tripulante serial NOT NULL 
 );
