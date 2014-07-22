9004584e59b60d418e1fff4e1e6ed99788215855
pnakibar <pedronakibar@gmail.com>
2014-07-21 21:14:17 -0300

    Populate tables sql added!!!!!!!!!

---
+++ Creational/Populate Tables.sql
@@ -0,0 +1,23 @@
+/*Popular tabela jogador*/
+
+INSERT INTO "jogador" (username,realname,password,email) VALUES ('The Iceman','Herman Clark','5174','Aliquam@Classaptent.org');
+INSERT INTO "jogador" (username,realname,password,email) VALUES ('Mr. Freeze','Carson Simmons','4537','ridiculus@mollis.org');
+INSERT INTO "jogador" (username,realname,password,email) VALUES ('The Monster','Riley Doyle','2325','luctus.Curabitur.egestas@ipsumsodalespurus.net');
+INSERT INTO "jogador" (username,realname,password,email) VALUES ('Crazy Legs','Harrison Fuentes','3309','Aliquam@sapienCrasdolor.net');
+INSERT INTO "jogador" (username,realname,password,email) VALUES ('Thunder','September Eaton','6127','fermentum.metus@idrisus.ca');
+INSERT INTO "jogador" (username,realname,password,email) VALUES ('Godzilla','Carter Mckee','7506','Suspendisse@magnaPraesentinterdum.org');
+INSERT INTO "jogador" (username,realname,password,email) VALUES ('Babyface','Justin Nunez','5570','morbi.tristique@mauriselitdictum.org');
+INSERT INTO "jogador" (username,realname,password,email) VALUES ('Bulldog','Xaviera Spencer','9220','ornare.tortor@convallis.org');
+INSERT INTO "jogador" (username,realname,password,email) VALUES ('The Assassin','Lionel Kennedy','8075','dolor@Proinnislsem.org');
+INSERT INTO "jogador" (username,realname,password,email) VALUES ('The Crippler','Steven Greene','6882','Nullam@sit.ca');
+INSERT INTO "jogador" (username,realname,password,email) VALUES ('The Nightmare','Felix Cervantes','1112','mattis.semper.dui@porttitor.org');
+INSERT INTO "jogador" (username,realname,password,email) VALUES ('Meltdown','Hoyt Cash','7027','quam.Pellentesque@tristique.net');
+INSERT INTO "jogador" (username,realname,password,email) VALUES ('King Kong','Mark Austin','9466','arcu@elit.com');
+INSERT INTO "jogador" (username,realname,password,email) VALUES ('Red Hot','Harding Sheppard','7971','porttitor.vulputate@Nullaeuneque.net');
+INSERT INTO "jogador" (username,realname,password,email) VALUES ('Bad Boy','Lacota Kinney','4585','Etiam.laoreet.libero@vulputateposuerevulputate.com');
+INSERT INTO "jogador" (username,realname,password,email) VALUES ('The Technician','Quamar Craig','8650','orci.lacus@lectussitamet.co.uk');
+INSERT INTO "jogador" (username,realname,password,email) VALUES ('Black Widow','Freya Valentine','8028','gravida.sit.amet@eleifendegestasSed.org');
+INSERT INTO "jogador" (username,realname,password,email) VALUES ('Gangsta','Quynn George','3314','Curae@Vivamus.co.uk');
+INSERT INTO "jogador" (username,realname,password,email) VALUES ('The Warhammer','Lionel Monroe','9878','scelerisque@aenimSuspendisse.org');
+INSERT INTO "jogador" (username,realname,password,email) VALUES ('Man of Stone','Hilda Price','8445','Cras.sed@sitamet.org');
+INSERT INTO "jogador" (username,realname,password,email) VALUES ('Sugar','Mallory Miller','7409','eget.metus.eu@egestasnunc.org');
