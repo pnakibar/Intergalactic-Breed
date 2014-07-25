098c54e42422e6092db14ffbd909fe4a292306e5
mateusdc21 <mateusdc21@gmail.com>
2014-07-25 14:33:45 -0300

    Query 3.1 no ponto

--- Querys/query3.1.sql
+++ Querys/query3.1.sql
@@ -2,31 +2,31 @@
 --/*
 UPDATE 	inimigo 
 SET 	nomeinimigo = 'Scarecrow' 
-WHERE 	nomeinimigo = 'Linus'
+WHERE 	nomeinimigo = 'Linus';
 --*/
 ---------------------------------
 --/*
 UPDATE 	inimigo 
 SET 	nomeinimigo = 'Spoder' 
-WHERE 	nomeinimigo = 'Kuame'
+WHERE 	nomeinimigo = 'Kuame';
 --*/
 ---------------------------------
 --/*
 UPDATE 	inimigo 
 SET 	nomeinimigo = 'Brikin' 
-WHERE 	nomeinimigo = 'Dale'
+WHERE 	nomeinimigo = 'Dale';
 --*/
 ---------------------------------
 --/*
 UPDATE 	inimigo 
 SET 	recompensa = 458
-WHERE 	recompensa = 433
+WHERE 	recompensa = 433;
 --*/
 ---------------------------------
 --/*
 UPDATE 	inimigo 
 SET 	recompensa = 193
-WHERE 	recompensa = 128 AND nomeinimigo = 'Cullen'
+WHERE 	recompensa = 128 AND nomeinimigo = 'Cullen';
 --*/
 ---------------------------------
 
@@ -35,31 +35,31 @@ WHERE 	recompensa = 128 AND nomeinimigo = 'Cullen'
 --/*
 UPDATE 	tripulante 
 SET 	tripulantename = 'Dakkor' 
-WHERE 	tripulantename = 'Mariam'
+WHERE 	tripulantename = 'Mariam';
 --*/
 ---------------------------------
 --/*
 UPDATE 	tripulante 
 SET 	tripulantename = 'Rhusi' 
-WHERE 	tripulantename = 'Teegan'
+WHERE 	tripulantename = 'Teegan';
 --*/
 ---------------------------------
 --/*
 UPDATE 	tripulante 
 SET 	idade = 17 
-WHERE 	idade = 40 AND tripulantename = 'Brenna'
+WHERE 	idade = 40 AND tripulantename = 'Brenna';
 --*/
 ---------------------------------
 --/*
 UPDATE 	tripulante 
 SET 	vida = 115 
-WHERE 	vida = 120 AND tripulantename = 'Zorita'
+WHERE 	vida = 120 AND tripulantename = 'Zorita';
 --*/
 ---------------------------------
 --/*
 UPDATE 	tripulante 
 SET 	id_funcao = 3 
-WHERE 	id_funcao = 1 AND tripulantename = 'Stuart'
+WHERE 	id_funcao = 1 AND tripulantename = 'Stuart';
 --*/
 ---------------------------------
 
@@ -68,30 +68,30 @@ WHERE 	id_funcao = 1 AND tripulantename = 'Stuart'
 --/*
 UPDATE 	tiponave 
 SET 	ataque = 128 
-WHERE 	ataque = 124
+WHERE 	ataque = 124;
 --*/
 ---------------------------------
 --/*
 UPDATE 	tiponave 
 SET 	ataque = 140, armadura = 450 
-WHERE 	nome = 'model03' AND armadura = 448
+WHERE 	nome = 'model03' AND armadura = 448;
 --*/
 ---------------------------------
 --/*
 UPDATE 	tiponave 
 SET 	nome = 'model91', ataque = 116 
-WHERE 	armadura = 448
+WHERE 	armadura = 448;
 --*/
 ---------------------------------
 --/*
 UPDATE 	tiponave 
 SET 	armadura = 500  
-WHERE 	nome = 'model04' AND ataque = 178
+WHERE 	nome = 'model04' AND ataque = 178;
 --*/
 ---------------------------------
 --/*
 UPDATE 	tiponave 
 SET 	nome = 'model70', armadura = 325, ataque = 102 
-WHERE 	ataque = 49
+WHERE 	ataque = 49;
 --*/
 ---------------------------------
