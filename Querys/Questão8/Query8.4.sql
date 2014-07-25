4b37c215b99732189d71ac581d298c92125efc85
pnakibar <pedronakibar@gmail.com>
2014-07-25 15:45:38 -0300

    querys8 terminadas

--- Querys/Questão8/Query8.4.sql
+++ Querys/Questão8/Query8.4.sql
@@ -1,3 +1,6 @@
 -- Query 8.4 --
-
-	 
+-- Mostra os tipos dos setores --
+SELECT 	s.id_setor AS "setor",
+		ts.nome AS "tipo"
+FROM 	setor s
+		INNER JOIN tiposetor ts ON s.id_tiposetor = ts.id_tiposetor
