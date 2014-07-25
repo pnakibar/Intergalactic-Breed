dc6bed91d90f738e5f6c15aa8bf85d53f09c2fc1
pnakibar <pedronakibar@gmail.com>
2014-07-25 16:17:50 -0300

    asdasd

--- scripts/makeViews.py
+++ scripts/makeViews.py
@@ -1,6 +1,22 @@
 import os
 
+def createHeader(fileName):
+    return "create or replace view "+fileName+" as"
+
+#get actual directory from OS
 curDir = os.getcwd()
-targetDir = curDir[:len(curDir)-7]
-targetDir+="queries"
-print(targetDir)
+
+#set target 'dir' to the desired directory
+targetDir = curDir[:len(curDir)-7]+"Querys\\"
+
+#desired folders to get opened
+desiredFolders = ['q5', 'q6','q7','q8']
+
+
+
+targetDir+=
+
+
+#abra as pastasDesejadas
+for e in pastasDesejadas:
+    print(os.listdir(targetDir+e))
